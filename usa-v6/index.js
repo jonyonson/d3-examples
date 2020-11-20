5a6e1d028ec4c6b017b12eda397c309a1879c64e
Jonathan Taylor <jonathan.taylor@daveramsey.com>
2020-11-20 16:24:15 -0600

    wip

--- usa-v6/index.js
+++ usa-v6/index.js
@@ -1,19 +1,39 @@
-// const projection = d3.geoEqualEarth();
-// const path = d3.geoPath(projection)
-
 const path = d3.geoPath();
 const svg = d3.select('body').append('svg').attr('viewBox', [0, 0, 975, 610]);
+// const svg = d3.create('svg').attr('viewBox', [0, 0, 975, 610]);
+
+// https://observablehq.com/@mbostock/u-s-state-map
+// https://observablehq.com/@d3/u-s-map
 
 d3.json('data/counties-albers-10m.json').then((us) => {
-  // console.log(topojson.feature(us));
-  console.log(topojson.feature(us, us.objects.nation).features);
+  // svg
+  //   .append('path')
+  //   .datum(topojson.feature(us, us.objects.nation))
+  //   .attr('fill', '#e0e0e0')
+  //   .attr('d', path);
+
+  // svg
+  //   .append('path')
+  //   .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
+  //   .attr('fill', 'none')
+  //   .attr('stroke', 'white')
+  //   .attr('stroke-linejoin', 'round')
+  //   .attr('d', path);
+
   svg
+    .append('g')
+    .attr('fill', '#ccc')
+    .selectAll('path')
+    .data(topojson.feature(us, us.objects.states).features)
+    .enter()
     .append('path')
-    .datum(topojson.feature(us, us.objects.nation))
-    .attr('fill', '#e0e0e0')
     .attr('d', path)
-    .on('click', (d) => {
-      console.log(d.properties);
+    .on('click', (_, d) => {
+      console.log(d.properties.name);
+      // const node = svg.node();
+      // node.value = value = value === d.id ? null : d.id;
+      // node.dispatchEvent(new CustomEvent('input'));
+      // outline.attr('d', value ? path(d) : null);
     });
 
   svg
@@ -22,16 +42,13 @@ d3.json('data/counties-albers-10m.json').then((us) => {
     .attr('fill', 'none')
     .attr('stroke', 'white')
     .attr('stroke-linejoin', 'round')
+    .attr('pointer-events', 'none')
     .attr('d', path);
 
-  const blah = topojson.mesh(us, us.objects.states, (a, b) => a !== b);
-  console.log(us);
-
-  // .on('click', (d) => {
-  //   console.log(d);
-  // });
-
-  // .on('click', (e) => {
-  //   console.log(e);
-  // });
+  const outline = svg
+    .append('path')
+    .attr('fill', 'none')
+    .attr('stroke', 'black')
+    .attr('stroke-linejoin', 'round')
+    .attr('pointer-events', 'none');
 });
